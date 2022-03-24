14cc3010daeaea48ea6416040232a07fe86da295
Sergey Morozov <se.mrzv@gmail.com>
2022-03-24 15:53:05 +0300

    show pokemons on mainpage map

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,7 +4,7 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='pokemons', blank=True)
+    photo = models.ImageField(upload_to='pokemons', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -16,9 +16,9 @@ class PokemonEntity(models.Model):
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True)
     disappeared_at = models.DateTimeField(null=True)
-    level = models.IntegerField(null=True)
-    health = models.IntegerField(null=True)
-    strength = models.IntegerField(null=True)
-    defence = models.IntegerField(null=True)
-    stamina = models.IntegerField(null=True)
+    level = models.IntegerField(default=50, null=True, blank=True,)
+    health = models.IntegerField(default=50, null=True, blank=True,)
+    strength = models.IntegerField(default=50, null=True, blank=True,)
+    defence = models.IntegerField(default=50, null=True, blank=True,)
+    stamina = models.IntegerField(default=50, null=True, blank=True,)
 
