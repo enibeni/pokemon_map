8b4c211a3cd7e210a43fca716def77e23c007589
Sergey Morozov <se.mrzv@gmail.com>
2022-03-26 16:44:49 +0300

    show exact type of pokemons on map

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,22 +3,25 @@ from django.db import models  # noqa F401
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
+    title_ru = models.CharField(max_length=200, null=True)
+    title_en = models.CharField(max_length=200, null=True, blank=True)
+    title_jp = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     photo = models.ImageField(upload_to='pokemons', blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return self.title_ru
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="entities")
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True)
     disappeared_at = models.DateTimeField(null=True)
-    level = models.IntegerField(default=50, null=True, blank=True,)
-    health = models.IntegerField(default=50, null=True, blank=True,)
-    strength = models.IntegerField(default=50, null=True, blank=True,)
-    defence = models.IntegerField(default=50, null=True, blank=True,)
-    stamina = models.IntegerField(default=50, null=True, blank=True,)
+    level = models.IntegerField(default=50, null=True, blank=True)
+    health = models.IntegerField(default=50, null=True, blank=True)
+    strength = models.IntegerField(default=50, null=True, blank=True)
+    defence = models.IntegerField(default=50, null=True, blank=True)
+    stamina = models.IntegerField(default=50, null=True, blank=True)
 
